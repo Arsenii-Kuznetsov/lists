d7d71210cd7bb6cdf9ce64e507cbad3a765d34e4
Arsenii Kuznetsov <arskuz2007@mail.ru>
2025-11-30 15:36:28 +0300

    feat: add solution to the 5th task

---
+++ 5.py
@@ -0,0 +1,34 @@
+from random import randint
+
+try:
+    M = int(input('Введите натуральное число(M): '))
+    K = int(input('Введите натуральное число(K): '))
+except ValueError:
+    exit('Введены не натуральные числа')
+if M <= 0 or K <= 0:
+    exit('Размеры матриц должны быть положительными числами')
+matrix = [[randint(-9, 9) for j in range(K)] for i in range(M)]
+print(matrix)
+answer_list = []
+while len(matrix) != 0:
+    answer_list += matrix[0]
+    matrix.remove(matrix[0])
+    M -= 1
+    if len(matrix) == 0:
+        break
+    for i in range(M):
+        answer_list += [matrix[i][K - 1]]
+        matrix[i].remove(matrix[i][K - 1])
+    K -= 1
+    if len(matrix) == 0:
+        break
+    answer_list += reversed(matrix[M - 1])
+    matrix.remove(matrix[M - 1])
+    M -= 1
+    if len(matrix) == 0:
+        break
+    for i in range(M - 1, -1, -1):
+        answer_list += [matrix[i][0]]
+        matrix[i].remove(matrix[i][0])
+    M -= 1
+print(*answer_list)
