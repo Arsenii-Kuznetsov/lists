f41fd079376565af965cbb4fd3c96283a57e9d23
Arsenii Kuznetsov <arskuz2007@mail.ru>
2025-11-30 15:49:59 +0300

    perf(core): remove replace with pop

--- 5.py
+++ 5.py
@@ -12,23 +12,23 @@ print(matrix)
 answer_list = []
 while len(matrix) != 0:
     answer_list += matrix[0]
-    matrix.remove(matrix[0])
+    matrix.pop(0)
     M -= 1
     if len(matrix) == 0:
         break
     for i in range(M):
         answer_list += [matrix[i][K - 1]]
-        matrix[i].remove(matrix[i][K - 1])
+        matrix[i].pop()
     K -= 1
     if len(matrix) == 0:
         break
     answer_list += reversed(matrix[M - 1])
-    matrix.remove(matrix[M - 1])
+    matrix.pop()
     M -= 1
     if len(matrix) == 0:
         break
     for i in range(M - 1, -1, -1):
         answer_list += [matrix[i][0]]
-        matrix[i].remove(matrix[i][0])
+        matrix[i].pop(0)
     M -= 1
 print(*answer_list)
